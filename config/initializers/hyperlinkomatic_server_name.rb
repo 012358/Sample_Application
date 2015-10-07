39fe6533dd223c47aaefefdd6a9f244232ffe4dc
Sajjad Murtaza (01770) <sajjad.murtaza@nxb.com.pk>
2015-10-07 16:51:55 +0500

    Place http with app.name

--- config/initializers/hyperlinkomatic_server_name.rb
+++ config/initializers/hyperlinkomatic_server_name.rb
@@ -1,5 +1,6 @@
-if Rails.env == 'development'
-  ENV['hyperlinkomatic_server_name'] = 'http://localhost:3009'
-else
-  ENV['hyperlinkomatic_server_name'] = 'hyperlinkomatic.herokuapp.com'
-end
+# if Rails.env == 'development'
+#   ENV['hyperlinkomatic_server_name'] = 'http://localhost:3009'
+# else
+#   ENV['hyperlinkomatic_server_name'] = 'http://hyperlinkomatic.herokuapp.com/'
+# end
+ENV['hyperlinkomatic_server_name'] = 'http://hyperlinkomatic.herokuapp.com'
